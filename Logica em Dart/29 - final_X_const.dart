b62e6111b9ba702fe2c7aacd928eb1a6ccdd9a78
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-01-15 22:20:50 -0300

    aula 29

--- Logica em Dart/29 - final_X_const.dart
+++ Logica em Dart/29 - final_X_const.dart
@@ -1,3 +1,10 @@
 void main() {
-  
+  const String nome = 'Lucas';
+  const int idade = 50 + 30;
+  const List<String> lista = ['lucas', 'henrique'];
+  //? Const é um valor que após ser declarado não poder alterado
+
+  final DateTime agora = DateTime.now();
+  //? finaly é util declarar quando n se saber o valor da var
+  //? até que o codigo seja rodado
 }
